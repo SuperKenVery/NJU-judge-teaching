3be1c85fafaf7e45f54bc9b57cf46979c341a057
ken <>
2025-06-18 20:40:59 +0800

    跳过已录入成绩无法评教的项目
    重构代码，简化xpath查询

--- judge.js
+++ judge.js
@@ -2,97 +2,102 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function allgood() {
+function xpath(query, index = 1) {
   let matches = document.evaluate(
-    '//div[@id="txwj-index-card"]//label[text()[contains(.,"很好")]]//input[@type="radio"]',
+    query,
     document,
     null,
     XPathResult.ANY_TYPE,
     null,
   );
+  let result;
+  for (let _i = 0; _i < index; _i++) {
+    result = matches.iterateNext();
+  }
+  return result;
+}
 
-  let good = matches.iterateNext();
-  while (good) {
+function allgood() {
+  let index = 1;
+  let good = xpath(
+    '//div[@id="txwj-index-card"]//label[text()[contains(.,"很好")]]//input[@type="radio"]',
+    index,
+  );
+
+  while (good != null) {
     good.checked = true;
-    good = matches.iterateNext();
+
+    index++;
+    good = xpath(
+      '//div[@id="txwj-index-card"]//label[text()[contains(.,"很好")]]//input[@type="radio"]',
+      index,
+    );
   }
 }
 
 async function submit() {
-  let submit = document
-    .evaluate(
-      '//footer[@id="txwjFooter"]//a[text()[contains(.,"提交")]]',
-      document,
-      null,
-      XPathResult.ANY_TYPE,
-      null,
-    )
-    .iterateNext();
+  let submit = xpath(
+    '//footer[@id="txwjFooter"]//a[text()[contains(.,"提交")]]',
+  );
   submit.click();
 
   await sleep(200);
 
-  let confirm = document
-    .evaluate(
-      '//a[text()[contains(.,"确认")]]',
-      document,
-      null,
-      XPathResult.ANY_TYPE,
-      null,
-    )
-    .iterateNext();
+  let confirm = xpath('//a[text()[contains(.,"确认")]]');
   if (confirm) confirm.click();
 
   await sleep(4000);
 
-  let no_recommend = document
-    .evaluate(
-      '//div[@id="buttons"]//button[text()[contains(.,"暂不推荐")]]',
-      document,
-      null,
-      XPathResult.ANY_TYPE,
-      null,
-    )
-    .iterateNext();
+  let no_recommend = xpath(
+    '//div[@id="buttons"]//button[text()[contains(.,"暂不推荐")]]',
+  );
   if (no_recommend) no_recommend.click();
 
   await sleep(200);
 
-  let no_tas = document
-    .evaluate(
-      '//div[@class="bh-dialog-btnContainerBox"]//a[text()[contains(.,"暂时不评")]]',
-      document,
-      null,
-      XPathResult.ANY_TYPE,
-      null,
-    )
-    .iterateNext();
+  let no_tas = xpath(
+    '//div[@class="bh-dialog-btnContainerBox"]//a[text()[contains(.,"暂时不评")]]',
+  );
   if (no_tas) no_tas.click();
 }
 
+// 为了跳过已经出成绩、无法评教的课。从1开始。
+let next_judge_button_index = 1;
 async function next() {
-  let next = document
-    .evaluate(
-      '//section[@class="ckdwpj"]//div[@class="card-btn blue"]',
-      document,
-      null,
-      XPathResult.ANY_TYPE,
-      null,
-    )
-    .iterateNext();
-  if (next == null) {
+  let judge_next = xpath(
+    '//section[@class="ckdwpj"]//div[@style="display: block;"]//div[@class="card-btn blue"]',
+    next_judge_button_index,
+  );
+  if (judge_next == null) {
     return false;
   }
-  next.click();
+
+  judge_next.click();
+
+  // 如果有课已经出成绩了，就会无法评教。
+  // 检测是否为该情况
+  await sleep(700);
+  let cannot_judge = xpath('//div[@class="bh-tip-top-bar"]');
+  if (cannot_judge != null) {
+    next_judge_button_index += 1;
+    let close = xpath('//a[@class="bh-tip-closeIcon"]');
+    close.click();
+    await sleep(700);
+    return await next();
+  }
+
   return true;
 }
 
 async function main() {
+  next_judge_button_index = 1;
   while (await next()) {
     await sleep(1000);
     allgood();
     await submit();
   }
+
+  console.log("Done");
 }
 
 await main();
